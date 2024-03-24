b0019f4b2e39a248551fa9d1222e88a6e0ddf1f6
corwin-bell <corwin.m.bell@gmail.com>
2024-03-23 18:19:43 -0600

    calls to OpenAI and renders response

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React, { useState } from "react";
 import "./App.css";
-
 import { Box, Button, Container, Grid, Paper, TextField } from "@mui/material";
 import AutorenewIcon from '@mui/icons-material/Autorenew';
+import { OpenAI } from "openai";
+
 
 function App() {
   const [prompt, setPrompt] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [response, setResponse] = useState("");
-  async function handleSubmit() {}
+
+  // key can still be stolen if bundled code is inspected
+  // supposedly better to set up a proxy server
+  // https://www.smashingmagazine.com/2023/05/safest-way-hide-api-keys-react/
+  const openai = new OpenAI({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+
+  async function handleSubmit() {
+    setIsLoading(true);
+
+    try {
+      const completion = await openai.chat.completions.create({
+        messages: [
+          { 
+              role: "system", 
+              content: "You are a helpful assistant." 
+          },
+          { 
+              role: "user",
+              content: prompt,
+          },
+        ],
+        model: "gpt-3.5-turbo",
+        temperature: 1
+      });
+      setResponse(completion.choices[0].message.content);
+      setIsLoading(false);
+    } 
+    catch (e) {
+      alert("Error: ", e);
+      setIsLoading(false);
+    }
+  }
   return (
     <Container>
       <Box sx={{ width: "100%", mt: 4  }}>
